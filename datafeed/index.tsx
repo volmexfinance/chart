8bfc0f28447dbd91ce78530479d9274b6fbd6942
jono <jonathanng1122@gmail.com>
2023-03-27 16:41:45 -0700

    added top 25 assets

--- datafeed/index.tsx
+++ datafeed/index.tsx
@@ -311,118 +311,162 @@ async function getCryptoCompareKlines(
 }
 
 function getAllSymbols() {
-  const indexAssets = getTokenList('index', 80001)
-
-  const volmexSymbolsIV = indexAssets.map((index) => {
-    const symbol = index.symbol === 'ETH' ? 'EVIV' : 'BVIV'
-    return {
-      symbol: symbol,
-      full_name: symbol,
-      description: `${index.name} Implied Volatility Index`,
-      exchange: 'Volmex',
-      type: 'crypto',
-    }
-  })
-
-  const volmexSymbolsRV = indexAssets.map((index) => {
-    const symbol = index.symbol === 'ETH' ? 'EVRV' : 'BVRV'
-    return {
-      symbol: symbol,
-      full_name: symbol,
-      description: `${index.name} Realized Volatility Index`,
-      exchange: 'Volmex',
-      type: 'crypto',
-    }
-  })
-
-  const volmexSymbolsRP = indexAssets.map((index) => {
-    const symbol = index.symbol === 'ETH' ? 'EVRP' : 'BVRP'
-    return {
-      symbol: symbol,
-      full_name: symbol,
-      description: `${index.name} Risk Premium Index`,
-      exchange: 'Volmex',
-      type: 'crypto',
-    }
-  })
-
-  const volmexSymbolsVCORR = [
-    ...indexAssets.map((index) => {
-      const symbol = index.symbol === 'ETH' ? 'EVCORR3D' : 'BVCORR3D'
-      return {
-        symbol: symbol,
-        full_name: symbol,
-        description: `${index.name} Spot Volatility 3 Day Correlation Index`,
-        exchange: 'Volmex',
-        type: 'crypto',
-      }
-    }),
-    ...indexAssets.map((index) => {
-      const symbol = index.symbol === 'ETH' ? 'EVCORR1W' : 'BVCORR1W'
-      return {
-        symbol: symbol,
-        full_name: symbol,
-        description: `${index.name} Spot Volatility 1 Week Correlation Index`,
-        exchange: 'Volmex',
-        type: 'crypto',
-      }
-    }),
-    ...indexAssets.map((index) => {
-      const symbol = index.symbol === 'ETH' ? 'EVCORR2W' : 'BVCORR2W'
-      return {
-        symbol: symbol,
-        full_name: symbol,
-        description: `${index.name} Spot Volatility 2 Week Correlation Index`,
-        exchange: 'Volmex',
-        type: 'crypto',
-      }
-    }),
-    ...indexAssets.map((index) => {
-      const symbol = index.symbol === 'ETH' ? 'EVCORR1M' : 'BVCORR1M'
-      return {
-        symbol: symbol,
-        full_name: symbol,
-        description: `${index.name} Spot Volatility 1 Month Correlation Index`,
-        exchange: 'Volmex',
-        type: 'crypto',
-      }
-    }),
-    ...indexAssets.map((index) => {
-      const symbol = index.symbol === 'ETH' ? 'EVCORR2M' : 'BVCORR2M'
-      return {
-        symbol: symbol,
-        full_name: symbol,
-        description: `${index.name} Spot Volatility 2 Month Correlation Index`,
-        exchange: 'Volmex',
-        type: 'crypto',
-      }
-    }),
-    ...indexAssets.map((index) => {
-      const symbol = index.symbol === 'ETH' ? 'EVCORR3M' : 'BVCORR3M'
-      return {
-        symbol: symbol,
-        full_name: symbol,
-        description: `${index.name} Spot Volatility 3 Month Correlation Index`,
-        exchange: 'Volmex',
-        type: 'crypto',
-      }
-    }),
+  // const indexAssets = getTokenList('index', 80001)
+  const indexAssets = [
+    { symbol: 'ETH', name: 'Ethereum', onlyRVandRP: false },
+    { symbol: 'BTC', name: 'Bitcoin', onlyRVandRP: false },
+    { symbol: 'BNB', name: 'Binance', onlyRVandRP: true },
+    { symbol: 'XRP', name: 'XRP', onlyRVandRP: true },
+    { symbol: 'ADA', name: 'Cardano', onlyRVandRP: true },
+    { symbol: 'DOGE', name: 'Dogecoin', onlyRVandRP: true },
+    { symbol: 'MATIC', name: 'Polygon', onlyRVandRP: true },
+    { symbol: 'OKB', name: 'OKB', onlyRVandRP: true },
+    { symbol: 'SOL', name: 'SOL', onlyRVandRP: true },
+    { symbol: 'SHIB', name: 'Shiba Inu', onlyRVandRP: true },
+    { symbol: 'DOT', name: 'Polkadot', onlyRVandRP: true },
+    { symbol: 'LTC', name: 'Litecoin', onlyRVandRP: true },
+    { symbol: 'TRX', name: 'Tron', onlyRVandRP: true },
+    { symbol: 'AVAX', name: 'Avalanche', onlyRVandRP: true },
+    { symbol: 'UNI', name: 'Uniswap', onlyRVandRP: true },
+    { symbol: 'ATOM', name: 'Cosmos', onlyRVandRP: true },
+    { symbol: 'LINK', name: 'Chainlink', onlyRVandRP: true },
+    { symbol: 'LEO', name: 'LEO', onlyRVandRP: true },
+    { symbol: 'XMR', name: 'Monero', onlyRVandRP: true },
+    { symbol: 'ETC', name: 'Ethereum Classic', onlyRVandRP: true },
+    { symbol: 'BCH', name: 'Bitcoin Cash', onlyRVandRP: true },
+    { symbol: 'APT', name: 'Aptos', onlyRVandRP: true },
+    { symbol: 'XLM', name: 'Stellar', onlyRVandRP: true },
+    { symbol: 'LDO', name: 'Lido', onlyRVandRP: true },
   ]
+  const symbolList = [
+    'BTC',
+    'ETH',
+    'BNB',
+    'XRP',
+    'ADA',
+    'DOGE',
+    'MATIC',
+    'OKB',
+    'SOL',
+    'SHIB',
+    'DOT',
+    'LTC',
+    'TRX',
+    'AVAX',
+    'UNI',
+    'ATOM',
+    'TON',
+    'LINK',
+    'LEO',
+    'XMR',
+    'ETC',
+    'BCH',
+    'APT',
+    'XLM',
+    'LDO',
+  ]
+  const getVolmexSymbolRP = (baseSymbol: string, name: string) => {
+    return {
+      symbol: baseSymbol + 'VRP',
+      full_name: baseSymbol + 'VRP',
+      description: `${name} Realized Price Index`,
+      exchange: 'Volmex',
+      type: 'crypto',
+    }
+  }
 
-  const volmexSymbolsPerps: any[] = [] /*indexAssets.map((index) => ({
-    symbol: index.symbol,
-    full_name: index.symbol + ' Mark',
-    description: `${index.name} Volatility Index`,
-    exchange: 'VolmexPerps',
-    type: 'crypto',
-  }))*/
+  const getVolmexSymbolRV = (baseSymbol: string, name: string) => {
+    return {
+      symbol: baseSymbol + 'VRV',
+      full_name: baseSymbol + 'VRV',
+      description: `${name} Realized Volatility Index`,
+      exchange: 'Volmex',
+      type: 'crypto',
+    }
+  }
+  const getVolmexSymbolIV = (baseSymbol: string, name: string) => {
+    return {
+      symbol: baseSymbol + 'VIV',
+      full_name: baseSymbol + 'VIV',
+      description: `${name} Implied Volatility Index`,
+      exchange: 'Volmex',
+      type: 'crypto',
+    }
+  }
+  const getVolmexSymbolsVCORR = (baseSymbol: string, name: string) => {
+    return [
+      {
+        symbol: baseSymbol + 'VCORR3D',
+        full_name: baseSymbol + 'VCORR3D',
+        description: `${name} Spot Volatility 3 Day Correlation Index`,
+        exchange: 'Volmex',
+        type: 'crypto',
+      },
+      {
+        symbol: baseSymbol + 'VCORR1W',
+        full_name: baseSymbol + 'VCORR1W',
+        description: `${name} Spot Volatility 1 Week Correlation Index`,
+        exchange: 'Volmex',
+        type: 'crypto',
+      },
+      {
+        symbol: baseSymbol + 'VCORR2W',
+        full_name: baseSymbol + 'VCORR2W',
+        description: `${name} Spot Volatility 2 Week Correlation Index`,
+        exchange: 'Volmex',
+        type: 'crypto',
+      },
+      {
+        symbol: baseSymbol + 'VCORR1M',
+        full_name: baseSymbol + 'VCORR1M',
+        description: `${name} Spot Volatility 1 Month Correlation Index`,
+        exchange: 'Volmex',
+        type: 'crypto',
+      },
+      {
+        symbol: baseSymbol + 'VCORR2M',
+        full_name: baseSymbol + 'VCORR2M',
+        description: `${name} Spot Volatility 2 Month Correlation Index`,
+        exchange: 'Volmex',
+        type: 'crypto',
+      },
+      {
+        symbol: baseSymbol + 'VCORR3M',
+        full_name: baseSymbol + 'VCORR3M',
+        description: `${name} Spot Volatility 3 Month Correlation Index`,
+        exchange: 'Volmex',
+        type: 'crypto',
+      },
+    ]
+  }
+  const getVolmexSymbolsFromIndex = (index: { symbol: string; name: string }, onlyRVandRP: boolean) => {
+    const baseSymbol = index.symbol === 'ETH' ? 'E' : index.symbol === 'BTC' ? 'B' : index.symbol
+    const volmexSymbolIV = getVolmexSymbolIV(baseSymbol, index.name)
+    const volmexSymbolRV = getVolmexSymbolRV(baseSymbol, index.name)
+    const volmexSymbolRP = getVolmexSymbolRP(baseSymbol, index.name)
+    const volmexSymbolsVCORR = getVolmexSymbolsVCORR(baseSymbol, index.name)
 
-  const volmexSymbols = volmexSymbolsIV
-    .concat(volmexSymbolsRV)
-    .concat(volmexSymbolsRP)
-    .concat(volmexSymbolsVCORR)
-    .concat(volmexSymbolsPerps)
+    const volmexSymbolsPerps: any[] = [] /*indexAssets.map((index) => ({
+      symbol: index.symbol,
+      full_name: index.symbol + ' Mark',
+      description: `${index.name} Volatility Index`,
+      exchange: 'VolmexPerps',
+      type: 'crypto',
+    }))*/
 
+    if (onlyRVandRP) {
+      return [volmexSymbolRV, volmexSymbolRP]
+    } else {
+      return [volmexSymbolIV, volmexSymbolRV, volmexSymbolRP, ...volmexSymbolsVCORR, ...volmexSymbolsPerps]
+    }
+  }
+
+  const volmexSymbols = indexAssets.reduce<Array<string>>((acc, index) => {
+    const volmexSymbols = getVolmexSymbolsFromIndex(index, index.onlyRVandRP)
+    acc.push(...volmexSymbols)
+    return acc
+  }, [])
+  console.log({ volmexSymbols })
   const extraSymbols = [
     {
       symbol: 'ETH/USD',
