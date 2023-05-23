209af153d42151b2234db3a09aaed5a20c27e1a9
jono <jonathanng1122@gmail.com>
2023-05-23 14:21:29 -0700

    REVERT THIS, USING TESTNET ENDPOINT

--- datafeed/constants.ts
+++ datafeed/constants.ts
@@ -1,4 +1,4 @@
 export const apiBaseUrl =
-  typeof window !== `undefined` && window.location.hostname.includes('volmex.finance')
+  false && typeof window !== `undefined` && window.location.hostname.includes('volmex.finance')
     ? 'https://rest-v1.volmex.finance'
     : 'https://test-api.volmex.finance'
