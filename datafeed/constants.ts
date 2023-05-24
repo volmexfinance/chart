7ca39fab43862eedfe5d75241ba3fc983ccec60d
jono <jonathanng1122@gmail.com>
2023-05-24 06:58:26 -0700

    Revert "REVERT THIS, USING TESTNET ENDPOINT"
    This reverts commit 209af153d42151b2234db3a09aaed5a20c27e1a9.

--- datafeed/constants.ts
+++ datafeed/constants.ts
@@ -1,4 +1,4 @@
 export const apiBaseUrl =
-  false && typeof window !== `undefined` && window.location.hostname.includes('volmex.finance')
+  typeof window !== `undefined` && window.location.hostname.includes('volmex.finance')
     ? 'https://rest-v1.volmex.finance'
     : 'https://test-api.volmex.finance'
