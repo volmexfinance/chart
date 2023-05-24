c227dadf7e496e61f523ed93878593866c2408c7
Nick Chisiu <nickchisiu@Nicks-MacBook-Pro.local>
2023-05-24 15:37:29 +0300

    switch to prod url

--- datafeed/constants.ts
+++ datafeed/constants.ts
@@ -1,4 +1,5 @@
 export const apiBaseUrl =
-  false && typeof window !== `undefined` && window.location.hostname.includes('volmex.finance')
-    ? 'https://rest-v1.volmex.finance'
-    : 'https://test-api.volmex.finance'
+  typeof window !== `undefined` &&
+  window.location.hostname.includes("volmex.finance")
+    ? "https://rest-v1.volmex.finance"
+    : "https://test-api.volmex.finance";
