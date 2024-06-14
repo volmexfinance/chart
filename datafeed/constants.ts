1a09dd0dc7bd3297bb28a7c259e83a6deeb784a2
jonomnom <jonomnom5097@gmail.com>
2024-06-14 08:42:25 -0700

    add restUrl import

--- datafeed/constants.ts
+++ datafeed/constants.ts
@@ -1,4 +1,6 @@
+import { restUrl } from "../../tvchart.constants";
+
 export const apiBaseUrl =
   typeof window !== `undefined` && window.location.hostname.includes('volmex.finance')
-    ? 'https://rest-v1.volmex.finance'
+    ? restUrl
     : 'https://test-api.volmex.finance'
