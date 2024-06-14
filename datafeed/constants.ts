3d2a5529350eae1c005409c134b8064273bee330
jonomnom <jonomnom5097@gmail.com>
2024-06-14 08:47:06 -0700

    update dev environment

--- datafeed/constants.ts
+++ datafeed/constants.ts
@@ -1,6 +1,3 @@
 import { restUrl } from "../../tvchart.constants";
 
-export const apiBaseUrl =
-  typeof window !== `undefined` && window.location.hostname.includes('volmex.finance')
-    ? restUrl
-    : 'https://test-api.volmex.finance'
+export const apiBaseUrl = restUrl
